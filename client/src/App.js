d8d44ee446d80a46a6a6855cae3167f5d023c933
dev-edu <eduardo.n.silva9999@gmail.com>
2024-06-06 17:02:25 -0300

    Update settings

--- client/src/App.js
+++ client/src/App.js
@@ -100,7 +100,7 @@ function App() {
                   onToggle={toggleReminder}
                 />
               ) : (
-                "Nenhuma tarefa por aqui"
+                "Nenhuma tarefa no momento"
               )}
             </>
           )}
